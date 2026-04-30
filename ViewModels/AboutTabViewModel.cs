a6b91b043f1b923a6f72f83d20e66590fcbe969a
aihf <aihaifeng009@outlook.com>
2026-04-30 09:02:15 +0800

    Add bilingual Athena user guide and wire About GitHub link

--- ViewModels/AboutTabViewModel.cs
+++ ViewModels/AboutTabViewModel.cs
@@ -1,10 +1,16 @@
 using CommunityToolkit.Mvvm.Input;
+using Serilog;
+using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace Athena.UI.ViewModels;
 
 public partial class AboutTabViewModel : ViewModelBase
 {
+    private const string RepositoryUrl = "https://github.com/mehaifeng/AthenaAgent";
+    private readonly ILogger _logger = Log.ForContext<AboutTabViewModel>();
+
     [RelayCommand]
     private async Task CheckForUpdatesAsync() { await Task.CompletedTask; }
 
@@ -12,5 +18,20 @@ public partial class AboutTabViewModel : ViewModelBase
     private void OpenDocumentation() { }
 
     [RelayCommand]
-    private void OpenGitHub() { }
+    private void OpenGitHub()
+    {
+        try
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = RepositoryUrl,
+                UseShellExecute = true
+            });
+            _logger.Information("Opened GitHub repository: {RepositoryUrl}", RepositoryUrl);
+        }
+        catch (Exception ex)
+        {
+            _logger.Error(ex, "Failed to open GitHub repository: {RepositoryUrl}", RepositoryUrl);
+        }
+    }
 }
